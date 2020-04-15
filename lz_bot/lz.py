1ce6b4de4f50a2f4bec9ba33c0c775c83d190cfa
cvb <ccvb3291@gmail.com>
2020-04-15 19:57:48 +0300

    Use new type network

--- lz_bot/lz.py
+++ lz_bot/lz.py
@@ -1,25 +1,28 @@
 import os
 import pickle
+import random
 import re
 
 import attr
 from aiogram import Bot, types
 from returns.pipeline import is_successful
 from returns.result import Failure, Result, Success, safe
-import random
 
 import sodeep.nns.gru_ex as gru
+from sodeep.corpus.three import default_encoder
 
 
 @attr.s
 class Lz:
     bot: Bot = attr.ib()
     model = attr.ib()
+    enc = attr.ib()
     prob: float = attr.ib(0.1)
 
     @classmethod
     def build(cls, ckpt, bot, dp):
-        lz = cls(bot=bot, model=gru.load_model(ckpt))
+        enc = default_encoder()
+        lz = cls(bot=bot, model=gru.load_model(enc, ckpt), enc=enc)
 
         dp.register_message_handler(lz.cmd_prob, commands=["prob"])
         dp.register_message_handler(lz.handler, regexp='.*')
@@ -29,22 +32,26 @@ class Lz:
         me = await self.bot.me
         username = me["username"]
         print("got msg")
-        if random.random() < self.prob:
-            txt = gru.generate_text(self.model, message.text)
+        if message.chat.type == types.ChatType.PRIVATE:
+            txt = gru.generate_text(self.model, self.enc, message.text)
+            await message.reply(txt)
+        elif random.random() < self.prob:
+            txt = gru.generate_text(self.model, self.enc, message.text)
             await message.reply(txt)
 
-        if f"@{username}" in message.text:
+        elif f"@{username}" in message.text:
             print("resolving")
-            txt = gru.generate_text(self.model, message.text)
+            txt = gru.generate_text(self.model, self.enc, message.text)
             await message.reply(txt)
 
     async def cmd_prob(self, message: types.Message):
         args = message.get_args()
         # parts = args.split("=>")
 
-        print("cmd", repr(args))
         prob = safe_float(args)
-        if is_successful(prob):
+        if not args:
+            await message.reply(f"reply prob = {self.prob}")
+        elif is_successful(prob):
             self.prob = prob.unwrap()
             await message.reply(f"reply prob = {self.prob}")
         else:
